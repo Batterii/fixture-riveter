67ef9dc18be670be474da769c3a2e0253361f444
Noah Bogart <nbogart@batterii.com>
2020-06-09 17:10:19 -0400

    Add sequence test to integrations

--- test/integrations/create.ts
+++ test/integrations/create.ts
@@ -13,19 +13,25 @@ describe('#create', function() {
 		fb.setAdapter(new ObjectionAdapter());
 
 		fb.define(function() {
-			fb.factory('user', User, (f) => {
+			fb.factory('user', User, (f: any) => {
 				f.attr('name', () => 'Noah');
 				f.attr('age', () => 32);
+				f.sequence('email', (n: number) => `test${n}@foo.bar`);
 			});
 		});
 	});
 
 	it('inserts into the database', async function() {
 		const model = await fb.create('user', {attrs: {name: 'Bogart'}});
+		const model2 = await fb.create('user');
 
 		expect(model.id).to.exist;
 		expect(model.name).to.equal('Bogart');
+		expect(model.email).to.equal('test1@foo.bar');
+		expect(model2.email).to.equal('test2@foo.bar');
 		expect(model).to.be.an.instanceof(User);
+
+		expect(model.email).to.not.equal(model2.email);
 
 		const user = await User.query().findById(model.id);
 		expect(user.id).to.equal(model.id);
