dcbf74b1c59f132065e796452f020c634ad6053b
Noah Bogart <nbogart@batterii.com>
2020-06-10 13:24:13 -0400

    Clean up IntegerSequence test

--- test/unit/sequences/integer-sequence.ts
+++ test/unit/sequences/integer-sequence.ts
@@ -18,32 +18,31 @@ describe('IntegerSequence', function() {
 	describe('#reset', function() {
 		it('sets id correctly', function() {
 			const seq = new IntegerSequence('name');
+			seq.next();
+			seq.next();
+			expect(seq.index).to.equal(3);
 			seq.reset();
-			expect(seq.index).to.deep.equal(1);
+			expect(seq.index).to.equal(1);
 		});
 	});
 
 	describe('#increment', function() {
-		let seq: IntegerSequence;
-
-		beforeEach(function() {
-			seq = new IntegerSequence('name');
-			seq.index = 1;
-		});
-
 		it('increases singular value by 1', function() {
+			const seq = new IntegerSequence('name');
 			seq.index = 0;
 			seq.increment();
 			expect(seq.index).to.deep.equal(1);
 		});
 
 		it('increases singular value up to limit', function() {
+			const seq = new IntegerSequence('name');
 			seq.index = 3;
 			seq.increment();
 			expect(seq.index).to.deep.equal(4);
 		});
 
 		it('increases singular value up to limit', function() {
+			const seq = new IntegerSequence('name');
 			seq.index = 9;
 			seq.increment();
 			expect(seq.index).to.deep.equal(10);
@@ -51,32 +50,27 @@ describe('IntegerSequence', function() {
 	});
 
 	describe('#next', function() {
-		let seq: IntegerSequence;
-
-		beforeEach(function() {
-			seq = new IntegerSequence('name');
-			seq.index = 1;
-		});
-
-		it('returns single character at index when given single value', function() {
+		it('returns default index', function() {
+			const seq = new IntegerSequence('name');
 			const result = seq.next();
 			expect(result).to.equal(1);
 		});
 
 		it('uses index appropriately', function() {
+			const seq = new IntegerSequence('name');
 			seq.index = 4;
 			const result = seq.next();
 			expect(result).to.equal(4);
 		});
 
 		it('increments index after generating result', function() {
-			seq.index = 4;
-			const result = seq.next();
-			expect(result).to.equal(4);
-			expect(seq.index).to.deep.equal(5);
+			const seq = new IntegerSequence('name');
+			seq.next();
+			expect(seq.index).to.deep.equal(2);
 		});
 
 		it('increments correctly when called multiple times', function() {
+			const seq = new IntegerSequence('name');
 			seq.index = 4;
 			seq.next();
 			seq.next();
@@ -86,6 +80,7 @@ describe('IntegerSequence', function() {
 		});
 
 		it('uses available callback', function() {
+			const seq = new IntegerSequence('name');
 			seq.callback = (x: string) => x.toString();
 			const result = seq.next();
 			expect(result).to.equal('1');
