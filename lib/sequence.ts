af6f38b6d2889096ab77f43f088887aa68646061
Noah Bogart <nbogart@batterii.com>
2021-06-04 16:26:05 -0400

    Switch license to Mozilla Public License

--- lib/sequence.ts
+++ lib/sequence.ts
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 import {first, isFunction, isNumber, isObjectLike, isPlainObject, isString} from "lodash";
 
 export type SequenceCallback<C> = (result: C) => any;
--- test/acceptance/adapters.ts
+++ test/acceptance/adapters.ts
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 import {DefaultAdapter} from "../../lib/adapters/default-adapter";
 import {ObjectionAdapter} from "../../lib/adapters/objection-adapter";
 import {FixtureRiveter} from "../../lib/fixture-riveter";
